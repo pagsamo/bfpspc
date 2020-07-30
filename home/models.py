f9d417b5fc78ad97e862799c4dda3f2890a561ac
Christian Mark Bituin <bituin.chrismark@gmail.com>
2020-07-30 22:58:19 +0800

    set null on foreign key relationships to avoid messing up the data again

--- home/models.py
+++ home/models.py
@@ -24,7 +24,7 @@ class Investigator(models.Model):
     FirstName = models.CharField(max_length=100)
     MiddleName = models.CharField(max_length=100)
     LastName = models.CharField(max_length=100)
-    Rank = models.ForeignKey(InvestigatorRank,on_delete=models.CASCADE)
+    Rank = models.ForeignKey(InvestigatorRank,on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
         delimeter = ' '
@@ -48,8 +48,8 @@ class Incident(models.Model):
     FireOutDateTime = models.DateTimeField(null=True, blank=True)
     HouseNumber = models.CharField(max_length=255,blank=True)
     Street = models.CharField(max_length=255,blank=True)
-    Barangay = models.ForeignKey(Barangay, on_delete=models.CASCADE)
-    OccupancyType = models.ForeignKey(OccupancyType, on_delete=models.CASCADE)
+    Barangay = models.ForeignKey(Barangay, on_delete=models.SET_NULL, null=True)
+    OccupancyType = models.ForeignKey(OccupancyType, on_delete=models.SET_NULL, null=True)
     OwnerEstablishmentName = models.CharField(max_length=255)
     ALARM_LEVEL_CHOICES = [
         ('1','1'),
@@ -65,13 +65,13 @@ class Incident(models.Model):
     FinalDamageCost = models.IntegerField(default=0)
     Origin = models.CharField(max_length=255, blank=True)
     Cause = models.TextField(blank=True)
-    FireArsonInvestigator = models.ForeignKey(Investigator, on_delete=models.CASCADE)
+    FireArsonInvestigator = models.ForeignKey(Investigator, on_delete=models.SET_NULL, null=True)
     REMARKS_CHOICES = [
         ('closed', 'Closed'),
         ('under investigation', 'Under Investigation'),
     ]
 
-    Remarks = models.CharField(max_length=255, choices=REMARKS_CHOICES, default='closed')
+    Remarks = models.CharField(max_length=255, choices=REMARKS_CHOICES, default='closed', blank=True)
     Notes = models.TextField(default="", blank=True)
 
     def __str__(self):
