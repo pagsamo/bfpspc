323cd246b67efbf40513766d5e14b766b95f3df0
Christian Mark Bituin <bituin.chrismark@gmail.com>
2020-12-25 22:34:52 +0800

    phew so i needed initial values

--- home/misc.py
+++ home/misc.py
@@ -1,8 +1,8 @@
 from home.models import Incident
 
 incidents = Incident.objects.all()
-earliest = Incident.objects.earliest('DateTime')
-latest = Incident.objects.latest('DateTime')
+earliest = Incident.objects.earliest('DateCalled')
+latest = Incident.objects.latest('DateCalled')
 
 def month(i):
     switcher = {
@@ -23,13 +23,13 @@ def month(i):
 
 def overTime():
     overTime = {}
-    for x in range(earliest.DateTime.year, (latest.DateTime.year+1)):
-        if x == earliest.DateTime.year:
-            for y in range(earliest.DateTime.month, 13):
-                overTime.update({"%s %s" %(month(y), x): incidents.filter(DateTime__year=x, DateTime__month=y).count()})
-        elif x == latest.DateTime.year:
-            for y in range(1, (latest.DateTime.month+1)):
-                overTime.update({"%s %s" %(month(y), x): incidents.filter(DateTime__year=x, DateTime__month=y).count()})
+    for x in range(earliest.DateCalled.year, (latest.DateCalled.year+1)):
+        if x == earliest.DateCalled.year:
+            for y in range(earliest.DateCalled.month, 13):
+                overTime.update({"%s %s" %(month(y), x): incidents.filter(DateCalled__year=x, DateCalled__month=y).count()})
+        elif x == latest.DateCalled.year:
+            for y in range(1, (latest.DateCalled.month+1)):
+                overTime.update({"%s %s" %(month(y), x): incidents.filter(DateCalled__year=x, DateCalled__month=y).count()})
         else:
             for y in range(1, 13):
-                overTime.update({"%s %s" %(month(y), x): incidents.filter(DateTime__year=x, DateTime__month=y).count()})
+                overTime.update({"%s %s" %(month(y), x): incidents.filter(DateCalled__year=x, DateCalled__month=y).count()})
